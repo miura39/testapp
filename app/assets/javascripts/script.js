d7ff2c006bf57d3f392727f8e78ed61734f934c1
miura39 <miurast.miura.y@gmail.com>
2020-03-24 21:23:18 +0900

    shiftで先頭文字の削除

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -18,5 +18,7 @@ let list = ['test','test2','test3']
 list.push('追加が出来ます');
 // 配列の要素を削除する(Rubyの場合はpopメソッドの引数に数字を渡すことで「何個分要素を取り除くか」という数を指定できますが、JavaScriptのpopメソッドではそれができません。)
 list.pop();
+// 先頭の要素を取り除くためにshiftメソッドが利用できます。(こちらもpopメソッド同様、Rubyでは取り除く要素数を指定できますが、JavaScriptではできません。)
+list.shift();
 console.log(list);
 console.log(list[2]);
