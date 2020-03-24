e4d846867063295bb4ee32bbd5186af6258a474e
miura39 <miurast.miura.y@gmail.com>
2020-03-24 20:46:40 +0900

    On the way to completion

--- db/migrate/20200324112137_create_posts.rb
+++ db/migrate/20200324112137_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.text
+      t.text :text
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_03_24_112137) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
