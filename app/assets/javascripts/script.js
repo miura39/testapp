b2c2903d6a8653aac44f44496a40560921f240a2
miura39 <miurast.miura.y@gmail.com>
2020-03-24 21:14:27 +0900

    javascript条件分岐

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,3 +1,15 @@
 var name = '名前';
 window.alert(name + 'こんにちは');
 console.log("こんばんわ");
+
+let num = 60;
+
+if (num % 15 == 0) {
+  console.log(num + 'は3と5の倍数です');
+} else if (num % 3 == 0) {
+  console.log(num + 'は3の倍数です');
+} else if (num % 5 == 0) {
+  console.log(num + 'は5の倍数です');
+} else {
+  console.log(num + 'は3の倍数でも、5の倍数でもありません');
+}
